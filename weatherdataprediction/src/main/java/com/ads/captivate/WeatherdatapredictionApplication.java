1b0b0251fc95caba029e3d52ca77f2b57eafb875
Shubhangi Shrivastava <shubhangi.shrivastava91@gmail.com>
2020-02-07 17:30:31 -0500

    Initial User Mgmt Code #11

---
+++ weatherdataprediction/src/main/java/com/ads/captivate/WeatherdatapredictionApplication.java
@@ -0,0 +1,24 @@
+package com.ads.captivate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@SpringBootApplication
+@EntityScan(basePackageClasses = { WeatherdatapredictionApplication.class, Jsr310JpaConverters.class })
+public class WeatherdatapredictionApplication {
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherdatapredictionApplication.class, args);
+	}
+
+}
---
+++ weatherdataprediction/src/test/java/com/iub/sice/sga/codestorm/weatherdataprediction/WeatherdatapredictionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iub.sice.sga.codestorm.weatherdataprediction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherdatapredictionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
