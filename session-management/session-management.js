40079d2d102a951674a86c71029caf26cedc8cfd
Adithya Selvaprithiviraj <adithyaselv@gmail.com>
2020-02-09 21:48:12 -0500

    Add kafka consumer

--- session-management/session-management.js
+++ session-management/session-management.js
@@ -1,5 +1,14 @@
 const express = require('express')
 const app = express()
+const kafka = require("kafka-node")
+
+Consumer = kafka.Consumer,
+client = new kafka.KafkaClient()
+consumer = new Consumer(client, [{ topic: "session-management"}])
+
+consumer.on("message", function(message) {
+  console.log(message);
+});
  
 app.get('/', function (req, res) {
   res.send('Hello from session management')
