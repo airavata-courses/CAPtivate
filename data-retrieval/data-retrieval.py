2808d0c966107ac0478e76ca4fe3fd529a717e01
Adithya Selvaprithiviraj <adithyaselv@gmail.com>
2020-02-09 19:27:49 -0500

    Retrieves data for specified dates by listening to data-retrieval topic

--- data-retrieval/data-retrieval.py
+++ data-retrieval/data-retrieval.py
@@ -9,19 +9,23 @@ mclient = MongoClient(port=27014)
 db = mclient.foo2
 
 consumer = KafkaConsumer(
-    'test',
+    'data-retrieval',
      bootstrap_servers=['localhost:9092'],
-     value_deserializer=lambda x: x.decode('utf-8'))
+     value_deserializer=lambda x: json.loads(x.decode('utf-8')))
 
+headers = {'token':'tlOrjLlVkdmPriWTBeKteqdOURnHqvjL'}
+url ='https://www.ncdc.noaa.gov/cdo-web/api/v2/data'
 
 while True:
     m = consumer.poll()
-    headers = {'token':'tlOrjLlVkdmPriWTBeKteqdOURnHqvjL'}
-    url ='https://www.ncdc.noaa.gov/cdo-web/api/v2/data'
-    params = {'datasetid': 'GHCND', 'locationid':'ZIP:28801', 'startdate': '2010-05-01', 'enddate': '2010-05-01'}
+    # params = {'datasetid': 'GHCND', 'locationid':'ZIP:28801', 'startdate': '2010-05-01', 'enddate': '2010-05-01'}
+    params = {'datasetid': 'GHCND', 'units': 'metric'}
     if bool(m):
         _,v = m.popitem()
-        # v[0].value
+        params['locationid']= v[0].value['locationid']
+        params['startdate'] = v[0].value['startdate']
+        params['enddate'] = v[0].value['startdate']
+        
         r = requests.get(url, params = params, headers = headers)
         bar = json.loads(r.text)
         result = db.weather.insert_one(bar)
